7eabfba3b19d9ec8af0093b0ac8adf67b896d835
tom <tom@atguigu.com>
2018-09-29 11:13:01 +0800

    gge

---
+++ gmall-activemq/src/test/java/com/atguigu/activemq/GmallActivemqApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atguigu.activemq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GmallActivemqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
